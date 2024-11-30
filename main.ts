f39b5ba0d041df3c683235122f78455dbd79019f
taha15102010 <nmoslem32@gmail.com>
2024-11-30 11:19:08 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.UntilDone)
+basic.showString("BONJOUR")
+basic.showLeds(`
+    # . . . #
+    # . # . #
+    # # # # #
+    # . # . #
+    # . . . #
+    `)
